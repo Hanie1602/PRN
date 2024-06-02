2d5a38f7fdd5cb7750c56428df672bbbe526ab70
TDHanie <luongngocthuyduongvt@gmail.com>
2024-06-02 08:37:31 +0700

    OOP in C#
    Review object-oriented programming (OOP) with C#

---
+++ Lession02/StudentManagerV1/StudentManagerV6/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace StudentManagerV6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void PlayWithGetSet()
+        {
+            //Biến là 1 vùng RAM được đặt tên, chứa giá trị nào đó
+            //Khi chơi với biến là ta  chơi với vùng RAM, chơi với giá trị, chơi thông qua tên biến
+            //Chơi với vùng AM/với biến ta làm được 2 việc sau:
+            //READ VÙNG RAM, Read biến coi nó có value gì???
+            //WRITE VÙNG RAM, thay đổi calue của vùng RAM, của biến
+
+            int x = 10;          //Vùng RAM, xx mang giá trị 10 khởi đầu
+
+            //Hãy in ra giá tr5i của biến, của vùng RAM, READ(), GET()
+            //Sờ chạm tên biến, là sờ chạm value
+            Console.WriteLine("x: " + x);   //GetX() được x
+
+            //Sửa, đổi value, SET() SETTING()
+            //Tên biến   dấu bằng chính là sửa SET() Value
+            x = 305;    //cuối tháng cuối tuần          //SET() thay đổi
+            Console.WriteLine("x again: " + x);     //GetX() đc x
+
+
+        }
+    }
+}
---
+++ Lession02/StudentManagerV1/StudentManagerV9/Entities/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagerV9.Entities
+{
+    internal class Student
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Yob { get; set; }
+        public double Gpa { get; set; }
+        //RUNTIME tự sinh ra giúp a _id, _name, _yob, _gpa
+        //Lúc chạy App
+        //Giúp dân Dev viết code tiện hơn, tránh những code nhàm chán
+
+        //Lỡ quên cú pháp, gõ prop tab
+        //public int MyProperty { get; set; }
+        //Bên Java ko có trò này, muốn làm trò này phải cài thêm Dependency bên ngoài, thư viên ngoài - thư viện LOMBOK
+    }
+}

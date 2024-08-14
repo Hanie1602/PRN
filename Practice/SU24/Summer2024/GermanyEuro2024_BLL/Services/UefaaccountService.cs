d1408ece61b1f882b3d0bc932b5c887cff85c581
TDHanie <luongngocthuyduongvt@gmail.com>
2024-08-14 13:30:51 +0700

    Practice Sumer 2024

---
+++ Practice/SU24/Summer2024/GermanyEuro2024_BLL/Services/UefaaccountService.cs
@@ -0,0 +1,20 @@
+﻿using GermanyEuro2024_DAL.Entities;
+using GermanyEuro2024_DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GermanyEuro2024_BLL.Services
+{
+	public class UefaaccountService
+	{
+		private UefaaccountRepository _repository = new();
+
+		public Uefaaccount Authenticate (string username, string password)
+		{
+			return _repository.GetOne(username, password);
+		}
+	}
+}
---
+++ Practice/SU24/Summer2024/GermanyEuro2024_DAL/Repositories/FootballPlayerRepository.cs
@@ -0,0 +1,22 @@
+﻿using GermanyEuro2024_DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GermanyEuro2024_DAL.Repositories
+{
+	public class FootballPlayerRepository
+	{
+		private GermanyEuro2024DbContext _context;
+
+		public List<FootballPlayer> GetFootballPlayer()
+		{
+			_context = new GermanyEuro2024DbContext();
+
+			return _context.FootballPlayers.Include("FootballTeam").ToList();
+		}
+	}
+}

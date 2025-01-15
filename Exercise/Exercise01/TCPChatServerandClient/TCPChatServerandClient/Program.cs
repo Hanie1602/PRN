7753ac8e66d7bdd7ce0853a0b84f0325a43b0aa3
TDHanie <luongngocthuyduongvt@gmail.com>
2025-01-16 00:03:32 +0700

    Program

--- Exercise/Exercise01/TCPChatServerandClient/TCPChatServerandClient/Program.cs
+++ Exercise/Exercise01/TCPChatServerandClient/TCPChatServerandClient/Program.cs
@@ -1,29 +1,39 @@
-﻿using TCPChatServerandClient;
-
-class Program
+﻿namespace TCPChatServerandClient
 {
-	static void Main(string[] args)
+	public class Program
 	{
-		Console.WriteLine("Choose mode: [1] for Server, [2] for Client");
-		string mode = Console.ReadLine();
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Choose: [1] for Server, [2] for Client");
+			string mode = Console.ReadLine();
 
-		if (mode == "1")
-		{
-			ChatServer server = new ChatServer();
-			server.StartServer(5000);
-		}
-		else if (mode == "2")
-		{
-			ChatClient client = new ChatClient();
-			Console.WriteLine("Enter Server IP (default: 127.0.0.1): ");
-			string serverIp = Console.ReadLine();
-			if (string.IsNullOrWhiteSpace(serverIp)) serverIp = "127.0.0.1";
+			if (mode == "1")
+			{
+				Console.Write("Enter port to listen on: ");
+				int port = int.Parse(Console.ReadLine());
+				ChatServer server = new ChatServer();
+				server.StartServer(port);       //Lắng nghe trên port
+			}
+			else if (mode == "2")
+			{
+				ChatClient client = new ChatClient();
 
-			client.ConnectToServer(serverIp, 5000);
-		}
-		else
-		{
-			Console.WriteLine("Invalid option.");
+				//Nếu người dùng ko nhập hoặc nhập khoảng trắng, mặc định IP = 127.0.0.1
+				Console.Write("Enter Server IP (default: 127.0.0.1): ");
+				string serverIp = Console.ReadLine();
+				if (string.IsNullOrEmpty(serverIp))
+					serverIp = "127.0.0.1";
+
+				Console.Write("Enter Server port: ");
+				int port = int.Parse(Console.ReadLine());
+
+				//Kết nối Client đến Server trên địa chỉ IP, port
+				client.ConnectToServer(serverIp, port);
+			}
+			else
+			{
+				Console.WriteLine("Invalid option.");
+			}
 		}
 	}
 }

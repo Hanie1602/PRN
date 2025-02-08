86ca0e4a2c289a41f439af589844c06fe647a816
TDHanie <luongngocthuyduongvt@gmail.com>
2025-02-08 07:40:53 +0700

    Program

---
+++ Advanced-Lesson/Exercise/Exercise03/Smart_Parking_System_Async_Await_Assignment/Smart_Parking_System_Async_Await_Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Smart_Parking_System_Async_Await_Assignment
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			SmartParkingSystem parking = new SmartParkingSystem();
+			List<string> car = new List<string>() { "A123", "A333", "A321" };
+
+			Console.WriteLine("Smart Parking System is starting...\n");
+
+			foreach (string carItem in car)
+			{
+				await parking.CarEnterAsync("A123");
+			}
+
+			await Task.Delay(5000);
+
+			foreach (string carItem in car)
+			{
+				await parking.CarExitAsync("A123");
+			}
+
+			Console.WriteLine("Parking system transaction completed!\n");
+		}
+	}
+}

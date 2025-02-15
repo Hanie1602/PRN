18914c05ab40a04c9aeabe7c2e07c57ebadd589e
TDHanie <luongngocthuyduongvt@gmail.com>
2025-02-15 14:07:44 +0700

    Program

--- Advanced-Lesson/Exercise/Exercise05/OOP/SingletonPattern/Program.cs
+++ Advanced-Lesson/Exercise/Exercise05/OOP/SingletonPattern/Program.cs
@@ -1,9 +1,16 @@
-﻿using SingletonPattern;
+﻿using System.Runtime.CompilerServices;
 
-Thread t1 = new Thread(DemoClass.Run);
-Thread t2 = new Thread(DemoClass.Run);
-
-t1.Start();
-t2.Start();
-
-Console.WriteLine("Finish!");
+namespace SingletonPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(DemoClass.Run);
+            Thread t2 = new Thread(DemoClass.Run);
+            t1.Start();
+            t2.Start();
+            Console.WriteLine("Finish");
+        }
+    }
+}

6a7ea9dc23f4bdc90581605b7ffe846b1b0c3edf
TDHanie <luongngocthuyduongvt@gmail.com>
2025-02-16 11:57:34 +0700

    Program

--- Advanced-Lesson/Exercise/Exercise02/SimpleChatApplication/ChatServer/Program.cs
+++ Advanced-Lesson/Exercise/Exercise02/SimpleChatApplication/ChatServer/Program.cs
@@ -1,13 +1,12 @@
-﻿
-namespace Server
+﻿namespace Server
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            ChatServer server = new ChatServer();
-            server.StartServer(5000);       //Lắng nghe trên port
-           
-        }
-    }
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			ChatServer server = new ChatServer();
+			server.StartServer(5000);       //Lắng nghe trên port
+
+		}
+	}
 }

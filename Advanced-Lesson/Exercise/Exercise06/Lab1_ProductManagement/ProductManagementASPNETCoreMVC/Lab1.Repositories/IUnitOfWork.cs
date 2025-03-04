16586c76c3745e975d3bc27a9907eaae99639403
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-04 23:57:54 +0700

    IUnitOfWork

---
+++ Advanced-Lesson/Exercise/Exercise06/Lab1_ProductManagement/ProductManagementASPNETCoreMVC/Lab1.Repositories/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace PRN222.Lab1.Repositories
+{
+	public interface IUnitOfWork : IDisposable
+	{
+		void Save();
+		Task SaveAsync();
+		IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
+	}
+}

e4015db453428abda91e106ba61161c633d8476d
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-04 23:57:02 +0700

    Product Service

--- Advanced-Lesson/Exercise/Exercise06/Lab1_ProductManagement/ProductManagementASPNETCoreMVC/Lab1.Services/ProductService.cs
+++ Advanced-Lesson/Exercise/Exercise06/Lab1_ProductManagement/ProductManagementASPNETCoreMVC/Lab1.Services/ProductService.cs
@@ -1,41 +1,72 @@
-﻿using PRN222.Lab1.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using PRN222.Lab1.Repositories;
 using PRN222.Lab1.Repositories.Entities;
 
 namespace PRN222.Lab1.Services
 {
 	public class ProductService : IProductService
     {
-        private readonly IProductRepository _repository;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public ProductService()
+        public ProductService(IUnitOfWork unitOfWork)
         {
-            _repository = new ProductRepository();
+            _unitOfWork = unitOfWork;
         }
 
         public void DeleteProduct(Product p)
         {
-            _repository.DeleteProduct(p);
-        }
+			try
+			{
+				_unitOfWork.GetRepository<Product>().Delete(p);
+				_unitOfWork.Save();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Error Deleting product: {ex.Message}");
+			}
+		}
 
 		public void SaveProduct(Product p)
 		{
-			_repository.SaveProduct(p);
+			try
+			{
+				_unitOfWork.GetRepository<Product>().InsertAsync(p);
+				_unitOfWork.Save();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Error Adding product: {ex.Message}");
+			}
 		}
 
 		public void UpdateProduct(Product p)
 		{
-			_repository.UpdateProduct(p);
+			try
+			{
+				_unitOfWork.GetRepository<Product>().Update(p);	
+				_unitOfWork.Save();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Error updating product: {ex.Message}");
+			}
 		}
 
 		public Product GetProductById(int id)
         {
-            return _repository.GetProductById(id);
-        }
+			return _unitOfWork.GetRepository<Product>().Entities.Include(p => p.Category).FirstOrDefault(c => c.ProductId.Equals(id));
+		}
 
         public List<Product> GetProducts()
         {
-            return _repository.GetProducts();
-        }
+			List<Product> listProducts = new List<Product>();
+			try
+			{
+				listProducts = _unitOfWork.GetRepository<Product>().Entities.Include(p => p.Category).ToList();
+			}
+			catch (Exception ex) { }
+			return listProducts;
+		}
 
     }
 }

897ffa6275dce8e65d39e8d1839df8b57fda3b02
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-04 23:58:13 +0700

    Interface Generic Repository

---
+++ Advanced-Lesson/Exercise/Exercise06/Lab1_ProductManagement/ProductManagementASPNETCoreMVC/Lab1.Repositories/IGenericRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace PRN222.Lab1.Repositories
+{
+	public interface IGenericRepository<TEntity> where TEntity : class
+	{
+		IQueryable<TEntity> Entities { get; }
+
+		IEnumerable<TEntity> GetAll();
+
+		Task<IList<TEntity>> GetAllAsync();
+
+		void Insert(TEntity obj);
+
+		Task InsertAsync(TEntity obj);
+
+		void Update(TEntity obj);
+
+		Task UpdateAsync(TEntity obj);
+
+		void Delete(Object id);
+
+		Task DeleteAsync(Object id);
+
+		void Save();
+
+		Task SaveAsync();
+	}
+}

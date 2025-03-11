bbb29db18a51b89aefa2e61423cb5671f7a12674
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-11 23:10:49 +0700

    Interface Product Service

---
+++ Advanced-Lesson/Exercise/Exercise06/Lab2/ProductManagementASPNETCoreRazorPages/PRN222.Lab2.Services/IService/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using PRN222.Lab2.Repositories.Entities;
+
+namespace PRN222.Lab2.Services.IService
+{
+	public interface IProductService
+	{
+		void SaveProduct(Product p);
+
+		void DeleteProduct(Product p);
+
+		void UpdateProduct(Product p);
+
+		List<Product> GetProducts();
+
+		Product GetProductById(int id);
+	}
+}

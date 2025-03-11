ae675a99f2b0726cb8c4afa5540b888b12cb45bb
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-11 23:08:05 +0700

    Product Create

---
+++ Advanced-Lesson/Exercise/Exercise06/Lab2/ProductManagementASPNETCoreRazorPages/PRN222.Lab2.RazorPages/Pages/Products/Create.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.SignalR;
+using PRN222.Lab2.Repositories.Entities;
+using PRN222.Lab2.Services.IService;
+using PRN222.Lab2.Services.Service;
+
+namespace PRN222.Lab2.RazorPages.Pages.Products
+{
+	public class CreateModel : PageModel
+    {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+		private readonly IHubContext<SignalrServer> _hubContext;
+
+		public CreateModel(IProductService productService, ICategoryService categoryService, IHubContext<SignalrServer> hubContext)
+        {
+            _productService = productService;
+            _categoryService = categoryService;
+            _hubContext = hubContext;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["CategoryId"] = new SelectList(_categoryService.GetCategories(), "CategoryId", "CategoryName");
+            return Page();
+        }
+
+        [BindProperty]
+        public Product Product { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _productService.SaveProduct(Product);
+			await _hubContext.Clients.All.SendAsync("LoadAllItems"); //Gửi tín hiệu cho All Client
+
+			return RedirectToPage("./Index");
+        }
+    }
+}

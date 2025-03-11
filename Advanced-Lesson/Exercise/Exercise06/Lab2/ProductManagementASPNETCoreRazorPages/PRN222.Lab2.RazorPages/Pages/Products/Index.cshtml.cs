a24ebac59b826a617ee56a10eff9e9a36f99d79d
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-11 23:06:48 +0700

    Index

---
+++ Advanced-Lesson/Exercise/Exercise06/Lab2/ProductManagementASPNETCoreRazorPages/PRN222.Lab2.RazorPages/Pages/Products/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PRN222.Lab2.Repositories.Entities;
+using PRN222.Lab2.Services.IService;
+
+namespace PRN222.Lab2.RazorPages.Pages.Products
+{
+	public class IndexModel : PageModel
+    {
+        private readonly IProductService _productService;
+
+        public IndexModel(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IList<Product> Product { get;set; } = default!;
+
+		public async Task<IActionResult> OnGet()
+		{
+			//Đọc Cookie xem User đã đăng nhập chưa
+			if (Request.Cookies["Account"] != null)
+			{
+				Product = _productService.GetProducts();
+				return Page();
+			}
+
+			return RedirectToPage("/Login");
+		}
+
+		public async Task<IActionResult> OnPostLogout()
+		{
+			Response.Cookies.Delete("Username");
+			Response.Cookies.Delete("Account");
+
+			return RedirectToPage("/Login");
+		}
+
+	}
+}

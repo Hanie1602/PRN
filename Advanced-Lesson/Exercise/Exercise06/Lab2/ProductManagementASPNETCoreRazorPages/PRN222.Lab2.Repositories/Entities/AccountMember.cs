377c4bd479a6b94de876e65994e768fd5e9c0b35
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-11 23:09:29 +0700

    Account Member

---
+++ Advanced-Lesson/Exercise/Exercise06/Lab2/ProductManagementASPNETCoreRazorPages/PRN222.Lab2.Repositories/Entities/AccountMember.cs
@@ -0,0 +1,15 @@
+﻿namespace PRN222.Lab2.Repositories.Entities
+{
+	public class AccountMember
+	{
+		public string MemberId { get; set; } = null!;
+
+		public string MemberPassword { get; set; } = null!;
+
+		public string FullName { get; set; } = null!;
+
+		public string EmailAddress { get; set; } = null!;
+
+		public int? MemberRole { get; set; }
+	}
+}

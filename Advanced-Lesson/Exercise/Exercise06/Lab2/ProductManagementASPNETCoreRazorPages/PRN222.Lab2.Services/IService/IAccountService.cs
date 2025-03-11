ceaf58a737663d610749063d1c0525642bb5de5c
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-11 23:10:33 +0700

    Interface Account Service

---
+++ Advanced-Lesson/Exercise/Exercise06/Lab2/ProductManagementASPNETCoreRazorPages/PRN222.Lab2.Services/IService/IAccountService.cs
@@ -0,0 +1,9 @@
+﻿using PRN222.Lab2.Repositories.Entities;
+
+namespace PRN222.Lab2.Services.IService
+{
+	public interface IAccountService
+	{
+		AccountMember GetAccountByEmail(string email);
+	}
+}

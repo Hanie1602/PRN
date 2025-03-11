67a15a72c7a3528558c9ce9ae31f89062e0c175f
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-11 23:11:51 +0700

    configuration

---
+++ Advanced-Lesson/Exercise/Exercise06/Lab2/ProductManagementASPNETCoreRazorPages/PRN222.Lab2.RazorPages/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿"use strict";
+var connection = new signalR.HubConnectionBuilder().withUrl("/signalRServer").build();
+
+connection.on("LoadAllItems", function () {
+    location.href = '/Products/Index';
+});
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});

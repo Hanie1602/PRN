1c42f653a46005233e8b909ba1fb4110f4de6710
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-11 23:10:07 +0700

    GenericRepository

---
+++ Advanced-Lesson/Exercise/Exercise06/Lab2/ProductManagementASPNETCoreRazorPages/PRN222.Lab2.Repositories/Data/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PRN222.Lab2.Repositories.Entities;
+
+namespace PRN222.Lab2.Repositories.Data
+{
+	public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+	{
+		protected readonly MyStoreDbContext _dbContext;
+		protected readonly DbSet<TEntity> _dbSet;
+
+		public GenericRepository(MyStoreDbContext dbContext)
+		{
+			_dbContext = dbContext;
+			_dbSet = dbContext.Set<TEntity>();
+		}
+
+		public IQueryable<TEntity> Entities => _dbSet;
+
+		public void Delete(object id)
+		{
+			TEntity entity = _dbSet.Find(id)!;
+			_dbSet.Remove(entity);
+		}
+
+		public IEnumerable<TEntity> GetAll()
+		{
+			return _dbSet.AsEnumerable();
+		}
+
+		public async Task<IList<TEntity>> GetAllAsync()
+		{
+			return await _dbSet.ToListAsync();
+		}
+
+		public void Insert(TEntity obj)
+		{
+			_dbSet.Add(obj);
+		}
+
+		public void Save()
+		{
+			_dbContext.SaveChanges();
+		}
+
+		public void Update(TEntity obj)
+		{
+			_dbSet.Entry(obj).State = EntityState.Modified;
+		}
+
+
+	}
+}

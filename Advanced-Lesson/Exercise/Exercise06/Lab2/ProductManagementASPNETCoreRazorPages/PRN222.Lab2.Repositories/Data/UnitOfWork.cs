c3abf8c6584b4b86a2863c110ce7671f05e4cc57
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-11 23:09:35 +0700

    Unit Of Work

---
+++ Advanced-Lesson/Exercise/Exercise06/Lab2/ProductManagementASPNETCoreRazorPages/PRN222.Lab2.Repositories/Data/UnitOfWork.cs
@@ -0,0 +1,53 @@
+﻿using PRN222.Lab2.Repositories.Entities;
+
+namespace PRN222.Lab2.Repositories.Data
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private bool disposed = false;
+		private readonly MyStoreDbContext _dbContext;
+		private readonly Dictionary<Type, object> _repositories;
+
+		public UnitOfWork(MyStoreDbContext dbContext)
+		{
+			_dbContext = dbContext;
+			_repositories = [];
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposed)
+			{
+				if (disposing)
+				{
+					Console.WriteLine("DbContext is being disposed.");
+					_dbContext.Dispose();
+				}
+			}
+			disposed = true;
+		}
+
+		public IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : class
+		{
+			if (_repositories.ContainsKey(typeof(TEntity)))
+			{
+				return (IGenericRepository<TEntity>)_repositories[typeof(TEntity)];
+			}
+
+			GenericRepository<TEntity> repository = new(_dbContext);
+			_repositories.Add(typeof(TEntity), repository);
+			return repository;
+		}
+
+		public void Save()
+		{
+			_dbContext.SaveChanges();
+		}
+	}
+}

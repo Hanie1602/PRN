c3ed89e96b3c02f3b16d8fd2ac00c5d33f8bcc0e
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-11 23:07:43 +0700

    Product Detail

---
+++ Advanced-Lesson/Exercise/Exercise06/Lab2/ProductManagementASPNETCoreRazorPages/PRN222.Lab2.RazorPages/Pages/Products/Details.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PRN222.Lab2.Repositories.Entities;
+using PRN222.Lab2.Services.IService;
+
+namespace PRN222.Lab2.RazorPages.Pages.Products
+{
+	public class DetailsModel : PageModel
+    {
+		private readonly IProductService _productService;
+
+		public DetailsModel(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public Product Product { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = _productService.GetProductById((int)id);
+			if (product == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Product = product;
+            }
+            return Page();
+        }
+    }
+}

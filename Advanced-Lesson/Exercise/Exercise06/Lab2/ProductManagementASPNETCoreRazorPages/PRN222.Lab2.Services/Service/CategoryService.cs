0fbdbc9674ab9c7e88a96dcc28b3f89edab1a8b0
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-11 23:12:28 +0700

    Category Service

---
+++ Advanced-Lesson/Exercise/Exercise06/Lab2/ProductManagementASPNETCoreRazorPages/PRN222.Lab2.Services/Service/CategoryService.cs
@@ -0,0 +1,31 @@
+﻿using PRN222.Lab2.Repositories.Data;
+using PRN222.Lab2.Repositories.Entities;
+using PRN222.Lab2.Services.IService;
+
+namespace PRN222.Lab2.Services.Service
+{
+	public class CategoryService : ICategoryService
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		public CategoryService(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public List<Category> GetCategories()
+		{
+			List<Category> listCategories = new List<Category>();
+			try
+			{
+				listCategories = _unitOfWork.GetRepository<Category>().Entities.ToList();
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+			return listCategories;
+		}
+
+	}
+}

b457b0e48b3e99dc7bcdd1455a0f5771fb6463dd
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-23 13:34:36 +0700

    Store Account

---
+++ Exam/PE_PRN222_SP25_TrialTest/PE_PRN221_FA24_000466/Repositories/Entities/StoreAccount.cs
@@ -0,0 +1,15 @@
+﻿namespace Repositories.Entities
+{
+	public class StoreAccount
+	{
+		public int StoreAccountId { get; set; }
+
+		public string StoreAccountPassword { get; set; } = null!;
+
+		public string EmailAddress { get; set; } = null!;
+
+		public string StoreAccountDescription { get; set; } = null!;
+
+		public int Role { get; set; }
+	}
+}

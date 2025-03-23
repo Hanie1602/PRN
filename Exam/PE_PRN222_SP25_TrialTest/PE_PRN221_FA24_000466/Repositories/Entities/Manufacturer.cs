326fef95accc310bd68bbe1026858b3bd099209c
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-23 13:34:18 +0700

    Manufacturer

---
+++ Exam/PE_PRN222_SP25_TrialTest/PE_PRN221_FA24_000466/Repositories/Entities/Manufacturer.cs
@@ -0,0 +1,19 @@
+﻿namespace Repositories.Entities
+{
+	public class Manufacturer
+	{
+		public string ManufacturerID { get; set; } = null!;
+
+		public string ManufacturerName { get; set; } = null!;
+
+		public string? ShortDescription { get; set; }
+
+		public int YearEstablished { get; set; }
+
+		public string ContactInformation { get; set; } = null!;
+
+		public string CountryofOrigin { get; set; } = null!;
+
+		public virtual ICollection<MedicineInformation> MedicineInformations { get; set; } = new List<MedicineInformation>();
+	}
+}

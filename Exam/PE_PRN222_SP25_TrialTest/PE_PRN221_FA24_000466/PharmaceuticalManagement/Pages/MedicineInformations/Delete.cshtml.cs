135520297b6110cf9a2a624783bf0269503f2a7d
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-23 13:33:11 +0700

    Delete

---
+++ Exam/PE_PRN222_SP25_TrialTest/PE_PRN221_FA24_000466/PharmaceuticalManagement/Pages/MedicineInformations/Delete.cshtml.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Entities;
+using Services.IService;
+
+namespace PharmaceuticalManagement.Pages.MedicineInformations
+{
+	public class DeleteModel : PageModel
+    {
+		private readonly IMedicineInformationService _medicineInfoService;
+		//private readonly IHubContext<SignalrServer> _hubContext;
+
+		public DeleteModel(IMedicineInformationService medicineInfoService)
+		{
+			_medicineInfoService = medicineInfoService;
+		}
+
+		[BindProperty]
+        public MedicineInformation MedicineInformation { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+			//Đọc Cookie xem User đã đăng nhập chưa
+			if (Request.Cookies["Account"] == null)
+			{
+				return RedirectToPage("/Login");
+			}
+
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			//Kiểm tra quyền
+			if (Request.Cookies["Account"] != "2")
+			{
+				TempData["PermissionError"] = "You do not have permission to do this function!";
+				return RedirectToPage("/MedicineInformations/Index");
+			}
+
+			MedicineInformation medicine = _medicineInfoService.GetMedicineInformationById((string)id);
+
+			if (medicine == null)
+			{
+				return NotFound();
+			}
+			else
+			{
+				MedicineInformation = medicine;
+			}
+			return Page();
+		}
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+			//Đọc Cookie xem User đã đăng nhập chưa
+			if (Request.Cookies["Account"] == null)
+			{
+				return RedirectToPage("/Login");
+			}
+
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			//Kiểm tra quyền
+			if (Request.Cookies["Account"] != "2")
+			{
+				TempData["PermissionError"] = "You do not have permission to do this function!";
+				return RedirectToPage("/MedicineInformations/Index");
+			}
+
+			MedicineInformation medicine = _medicineInfoService.GetMedicineInformationById((string)id);
+			if (medicine != null)
+			{
+				MedicineInformation = medicine;
+				_medicineInfoService.Delete(medicine);
+				//await _hubContext.Clients.All.SendAsync("LoadAllItems"); //Gửi tín hiệu cho All Client
+			}
+
+			return RedirectToPage("./Index");
+		}
+    }
+}

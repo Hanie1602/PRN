2f5f03c42e8f0c1e14d5a97f6cf5d91002dde8fc
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-23 13:36:50 +0700

    Manufacturer Service

---
+++ Exam/PE_PRN222_SP25_TrialTest/PE_PRN221_FA24_000466/Services/Service/ManufacturerService.cs
@@ -0,0 +1,23 @@
+﻿using Repositories.Entities;
+using Repositories.UOW;
+using Services.IService;
+
+namespace Services.Service
+{
+	public class ManufacturerService : IManufacturerService
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		public ManufacturerService(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public List<Manufacturer> GetManufactureres()
+		{
+			List<Manufacturer> listManufactureres = new List<Manufacturer>();
+			listManufactureres = _unitOfWork.GetRepository<Manufacturer>().Entities.ToList();
+			return listManufactureres;
+		}
+	}
+}

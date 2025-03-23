abef1bf9e8ff9329c4f3095d4e6fef9ecdd03b6d
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-23 13:35:10 +0700

    Unit Of Work

---
+++ Exam/PE_PRN222_SP25_TrialTest/PE_PRN221_FA24_000466/Repositories/UOW/UnitOfWork.cs
@@ -0,0 +1,55 @@
+﻿using Repositories.Entities;
+using Repositories.Generic_Repositories;
+
+namespace Repositories.UOW
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private bool disposed = false;
+		private readonly Sp25PharmaceuticalDBContext _dbContext;
+		private readonly Dictionary<Type, object> _repositories;
+
+		public UnitOfWork(Sp25PharmaceuticalDBContext dbContext)
+		{
+			_dbContext = dbContext;
+			_repositories = [];
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposed)
+			{
+				if (disposing)
+				{
+					Console.WriteLine("DbContext is being disposed.");
+					_dbContext.Dispose();
+				}
+			}
+			disposed = true;
+		}
+
+		public IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : class
+		{
+			if (_repositories.ContainsKey(typeof(TEntity)))
+			{
+				return (IGenericRepository<TEntity>)_repositories[typeof(TEntity)];
+			}
+
+			GenericRepository<TEntity> repository = new(_dbContext);
+			_repositories.Add(typeof(TEntity), repository);
+			return repository;
+		}
+
+		public void Save()
+		{
+			_dbContext.SaveChanges();
+		}
+
+	}
+}

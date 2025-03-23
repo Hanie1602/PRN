768555a07056ce0002e6a18201abf4d57e06025c
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-23 13:36:28 +0700

    Store Account Service

---
+++ Exam/PE_PRN222_SP25_TrialTest/PE_PRN221_FA24_000466/Services/Service/StoreAccountService.cs
@@ -0,0 +1,27 @@
+﻿using Repositories.Entities;
+using Repositories.UOW;
+using Services.IService;
+
+namespace Services.Service
+{
+	public class StoreAccountService : IStoreAccountService
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		public StoreAccountService(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public StoreAccount GetAccountByEmail(string email)
+		{
+			StoreAccount? account = _unitOfWork.GetRepository<StoreAccount>()
+				.Entities
+				.FirstOrDefault(c => c.EmailAddress.Equals(email))
+				?? throw new Exception("Account not found");
+
+			return account;
+		}
+
+	}
+}

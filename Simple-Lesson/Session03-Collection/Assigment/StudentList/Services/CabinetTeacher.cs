6ab30c144ae778ecaae1603cb7ed7afc37eabc6f
TDHanie <luongngocthuyduongvt@gmail.com>
2025-04-01 06:40:04 +0700

    Cabinet Teacher Service

---
+++ Simple-Lesson/Session03-Collection/Assigment/StudentList/Services/CabinetTeacher.cs
@@ -0,0 +1,51 @@
+﻿using StudentList.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentList.Services
+{
+	public class CabinetTeacher
+	{
+		private Teacher[] _arr = new Teacher[30];
+		private int _count = 0;
+
+		public int Count => _count;     //Hàm Get();
+
+		public int GetCount()
+		{
+			return _count;
+		}
+
+		public CabinetTeacher(int size)
+		{
+			_arr = new Teacher[size];
+		}
+
+		public CabinetTeacher()
+		{
+
+		}
+
+		public void PrintTeacherList()
+		{
+            Console.WriteLine($"The is/are {_count} teacher in the list");
+			for(int i = 0; i < _count; i++)
+				_arr[i].ShowProfile();
+        }
+
+		public void AddTeacher(Teacher t)
+		{
+			if (_count == 30)
+			{
+                Console.WriteLine("The list is full");
+				return;
+            }
+
+			_arr[_count] = t;		//new Teacher() {}
+			_count++;
+		}
+	}
+}

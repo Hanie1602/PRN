8e1aabf7a51fdf64cd635f647c26d36821b2ac9d
TDHanie <luongngocthuyduongvt@gmail.com>
2025-04-03 12:57:08 +0700

    Use Account Service

---
+++ Simple-Lesson/Session06-GUI/BookManagement_HoangNgocTrinh.8/BookManagement_HoangNgocTrinh.8/Services/UserAccountService.cs
@@ -0,0 +1,23 @@
+﻿using Repositories;
+using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+	public class UserAccountService
+	{
+		//GUI --> Service --> Repo --> DbContext --> Table
+		private UserAccountRepository _repo = new();
+
+		//CRUD UserAccount như bình thường, còn thêm hàm tính xem login sai mấy lần...
+		//Bài này mình chỉ tập trung vào login hoy
+		public UserAccount Authenticate(string email, string password)
+		{
+			return _repo.GetOne(email, password);
+		}
+	}
+}

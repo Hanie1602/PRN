f924f5a899f56b2c5b579319f147f765832d22d4
TDHanie <luongngocthuyduongvt@gmail.com>
2025-05-16 22:18:59 +0700

    Leaderboards DuongLNT Repository

---
+++ PRN232/Exercise/SU25_PRN232_SE1731_ASM1_SE171916_DuongLNT/SmokeQuit.Repositories.DuongLNT/LeaderboardsDuongLNTRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmokeQuit.Repositories.DuongLNT.Basic;
+using SmokeQuit.Repositories.DuongLNT.DBContext;
+using SmokeQuit.Repositories.DuongLNT.Models;
+
+namespace SmokeQuit.Repositories.DuongLNT
+{
+	public class LeaderboardsDuongLNTRepository : GenericRepository<LeaderboardsDuongLnt>
+	{
+		public LeaderboardsDuongLNTRepository() => _context ??= new DBContext.SU25_PRN232_SE1731_G6_SmokeQuitContext();
+
+		public LeaderboardsDuongLNTRepository(SU25_PRN232_SE1731_G6_SmokeQuitContext context) => _context = context;
+
+		public async Task<List<LeaderboardsDuongLnt>> GetAllAsync()
+		{
+			var termDepositSlips = await _context.LeaderboardsDuongLnts.Include(l => l.Plan).ToListAsync();
+
+			return termDepositSlips;
+		}
+
+		public async Task<LeaderboardsDuongLnt> GetById(int id)
+		{
+			var termDepositSlip = await _context.LeaderboardsDuongLnts.Include(d => d.Plan).FirstOrDefaultAsync(d => d.PlanId == id);
+
+			return termDepositSlip ?? new LeaderboardsDuongLnt();
+		}
+
+		//Search 3 điều kiện
+
+	}
+
+
+	
+}

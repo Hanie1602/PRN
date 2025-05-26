fe8ac2b2bf62225ca78b81c21859287b5a9d8c66
TDHanie <luongngocthuyduongvt@gmail.com>
2025-05-26 21:57:11 +0700

    LeaderboardsDuongLnt Service

---
+++ PRN232/Exercise/SU25_PRN232_SE1731_ASM1_SE171916_DuongLNT/SmokeQuit.Services.DuongLNT/LeaderboardsDuongLntService.cs
@@ -0,0 +1,52 @@
+﻿using SmokeQuit.Repositories.DuongLNT;
+using SmokeQuit.Repositories.DuongLNT.Models;
+
+namespace SmokeQuit.Services.DuongLNT
+{
+	//public interface ILeaderboardsDuongLntService
+	//{
+
+	//}
+
+	public class LeaderboardsDuongLntService : ILeaderboardsDuongLntService
+	{
+		private readonly LeaderboardsDuongLNTRepository _repository;
+		
+		//Nếu null, không tham số thì new nó
+		public LeaderboardsDuongLntService() => _repository ??= new LeaderboardsDuongLNTRepository();
+
+		public LeaderboardsDuongLntService(LeaderboardsDuongLNTRepository repository) => _repository = repository;
+
+		public async Task<int> CreateAsync(LeaderboardsDuongLnt leaderboards)
+		{
+			return await _repository.CreateAsync(leaderboards);
+		}
+
+		public async Task<bool> DeleteAsync(int id)
+		{
+			var leaderboards = await _repository.GetByIdAsync(id);
+			return await _repository.RemoveAsync(leaderboards);
+		}
+
+		public async Task<List<LeaderboardsDuongLnt>> GetAllAsync()
+		{
+			return await _repository.GetAllAsync();
+		}
+
+		public async Task<LeaderboardsDuongLnt> GetByIdAsync(int id)
+		{
+			return await _repository.GetByIdAsync(id);
+		}
+
+		public async Task<List<LeaderboardsDuongLnt>> SearchAsync(string note, double money, string reason)
+		{
+			return await _repository.SearchAsync(note, money, reason);
+		}
+
+		public async Task<int> UpdateAsync(LeaderboardsDuongLnt leaderboards)
+		{
+			return await _repository.UpdateAsync(leaderboards);
+		}
+
+	}
+}

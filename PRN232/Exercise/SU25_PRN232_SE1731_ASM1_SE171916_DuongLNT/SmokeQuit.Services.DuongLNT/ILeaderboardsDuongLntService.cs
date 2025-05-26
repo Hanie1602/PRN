cb32f2a63f51ee2203b91dcd2060bf363cb9c754
TDHanie <luongngocthuyduongvt@gmail.com>
2025-05-26 21:57:08 +0700

    Interface LeaderboardsDuongLnt Service

---
+++ PRN232/Exercise/SU25_PRN232_SE1731_ASM1_SE171916_DuongLNT/SmokeQuit.Services.DuongLNT/ILeaderboardsDuongLntService.cs
@@ -0,0 +1,18 @@
+﻿using SmokeQuit.Repositories.DuongLNT.Models;
+
+namespace SmokeQuit.Services.DuongLNT
+{
+	public interface ILeaderboardsDuongLntService
+	{
+		Task<List<LeaderboardsDuongLnt>> GetAllAsync();
+
+		Task<LeaderboardsDuongLnt> GetByIdAsync(int id);
+
+		Task<List<LeaderboardsDuongLnt>> SearchAsync(string note, double money, string reason);
+
+		Task<int> CreateAsync(LeaderboardsDuongLnt leaderboards);
+		Task<int> UpdateAsync(LeaderboardsDuongLnt leaderboards);
+		Task<bool> DeleteAsync(int id);
+
+	}
+}

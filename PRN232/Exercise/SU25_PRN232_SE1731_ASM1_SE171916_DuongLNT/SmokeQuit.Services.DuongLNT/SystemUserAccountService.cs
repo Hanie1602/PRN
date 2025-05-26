a5f923f8fb3590745be427c4bb9c8dd5dabd386c
TDHanie <luongngocthuyduongvt@gmail.com>
2025-05-26 21:57:33 +0700

    SystemUserAccount Service

---
+++ PRN232/Exercise/SU25_PRN232_SE1731_ASM1_SE171916_DuongLNT/SmokeQuit.Services.DuongLNT/SystemUserAccountService.cs
@@ -0,0 +1,18 @@
+﻿using SmokeQuit.Repositories.DuongLNT;
+using SmokeQuit.Repositories.DuongLNT.Models;
+
+namespace SmokeQuit.Services.DuongLNT
+{
+	public class SystemUserAccountService
+	{
+		private readonly SystemUserAccountRepository _repository;
+		public SystemUserAccountService() => _repository ??= new SystemUserAccountRepository();
+
+		public async Task<SystemUserAccount> GetUserAccount(string username, string password)
+		{
+			return await _repository.GetUserAccount(username, password);
+		}
+
+		
+	}
+}

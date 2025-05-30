7d1efcbb3e2b604113c5bb01a9d6b906c1ef9c52
TDHanie <luongngocthuyduongvt@gmail.com>
2025-05-30 20:16:11 +0700

    LeaderboardsDuongLNT Repository

--- PRN232/Exercise/SU25_PRN232_SE1731_ASM1_SE171916_DuongLNT/SmokeQuit.Repositories.DuongLNT/LeaderboardsDuongLNTRepository.cs
+++ PRN232/Exercise/SU25_PRN232_SE1731_ASM1_SE171916_DuongLNT/SmokeQuit.Repositories.DuongLNT/LeaderboardsDuongLNTRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SmokeQuit.Repositories.DuongLNT.Basic;
 using SmokeQuit.Repositories.DuongLNT.DBContext;
+using SmokeQuit.Repositories.DuongLNT.ModelExtensions;
 using SmokeQuit.Repositories.DuongLNT.Models;
 using System.IO.Pipes;
 
@@ -46,38 +47,89 @@ namespace SmokeQuit.Repositories.DuongLNT
 			//Nếu null thì bên phía Client sẽ không cho nó trống
 			return leaderboards ?? new List<LeaderboardsDuongLnt>();
 		}
-		
-		public async Task<dynamic> SearchWithPaggingAsc(string note, double money, string reason, int page, int pageSize)
+
+		//public async Task<dynamic> SearchWithPaggingAsc(string note, double money, string reason, int page, int pageSize)
+		//{
+		//	//Query by conditions
+		//	var leaderboards = await _context.LeaderboardsDuongLnts
+		//		.Include(l => l.Plan)
+		//		.Where(l =>
+		//			(l.Note.Contains(note) || string.IsNullOrEmpty(note))
+		//			&& (l.MoneySave == money || money == null || money == 0)
+		//			&& (l.Plan.Reason.Contains(reason)))
+		//		.ToListAsync();
+
+		//	//Pagging 
+		//	var totalCount = leaderboards.Count();
+		//	var totalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+
+		//	leaderboards = leaderboards.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+		//	var result = new
+		//	{
+		//		TotalItems = totalCount,
+		//		TotalPages = totalPages,
+		//		CurrentPage = page,
+		//		PageSize = pageSize,
+		//		LeaderBoardss = leaderboards
+		//	};
+
+		//	//Nếu null thì bên phía Client sẽ không cho nó trống
+		//	return result;
+		//}
+
+		public async Task<PaginationResult<List<LeaderboardsDuongLnt>>> SearchWithPagingAsync(string note, double money, string reason, int currentPage, int pageeSize)
 		{
 			//Query by conditions
-			var leaderboards = await _context.LeaderboardsDuongLnts
-				.Include(l => l.Plan)
-				.Where(l =>
-					(l.Note.Contains(note) || string.IsNullOrEmpty(note))
-					&& (l.MoneySave == money || money == null || money == 0)
-					&& (l.Plan.Reason.Contains(reason)))
-				.ToListAsync();
+			//var leaderboards = await _context.LeaderboardsDuongLnts
+			//	.Include(l => l.Plan)
+			//	.Where(l =>
+			//		(l.Note.Contains(note) || string.IsNullOrEmpty(note))
+			//		&& (l.MoneySave == money || money == null || money == 0)
+			//		&& (l.Plan.Reason.Contains(reason)))
+			//	.ToListAsync();
 
-			//Pagging 
-			var totalCount = leaderboards.Count();
-			var totalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+			var leaderboards = await this.SearchAsync(note, money, reason);
 
-			leaderboards = leaderboards.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+			//Paging
+			var totalItems = leaderboards.Count();
+			var totalPages = (int)Math.Ceiling((double)totalItems / pageeSize);
 
-			var result = new
+			leaderboards = leaderboards.Skip((currentPage - 1) * pageeSize).Take(pageeSize).ToList();
+
+			var result = new PaginationResult<List<LeaderboardsDuongLnt>>
 			{
-				TotalItems = totalCount,
+				TotalItems = totalItems,
 				TotalPages = totalPages,
-				CurrentPage = page,
-				PageSize = pageSize,
-				LeaderBoardss = leaderboards
+				CurrentPage = currentPage,
+				PageSize = pageeSize,
+				Items = leaderboards
 			};
 
-			//Nếu null thì bên phía Client sẽ không cho nó trống
 			return result;
 		}
+
+		public async Task<PaginationResult<List<LeaderboardsDuongLnt>>> GetAllWithPagingAsync(int currentPage, int pageSize)
+		{
+			var leaderboards = await this.GetAllAsync();
+
+			//Paging
+			var totalItems = leaderboards.Count();
+			var totalPages = (int)Math.Ceiling((double)totalItems / pageSize);
+
+			leaderboards = leaderboards.Skip((currentPage - 1) * pageSize).Take(pageSize).ToList();
+
+			var result = new PaginationResult<List<LeaderboardsDuongLnt>>
+			{
+				TotalItems = totalItems,
+				TotalPages = totalPages,
+				CurrentPage = currentPage,
+				PageSize = pageSize,
+				Items = leaderboards
+			};
+			return result;
+		}
+
 	}
-
-
 
 }

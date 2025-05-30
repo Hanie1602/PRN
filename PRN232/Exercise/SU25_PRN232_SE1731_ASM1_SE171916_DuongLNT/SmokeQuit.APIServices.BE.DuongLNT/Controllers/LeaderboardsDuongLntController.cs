50b13816840b9e88470ed13c29716ef7c7a1bc1c
TDHanie <luongngocthuyduongvt@gmail.com>
2025-05-30 20:11:26 +0700

    Leaderboards DuongLnt Controller

--- PRN232/Exercise/SU25_PRN232_SE1731_ASM1_SE171916_DuongLNT/SmokeQuit.APIServices.BE.DuongLNT/Controllers/LeaderboardsDuongLntController.cs
+++ PRN232/Exercise/SU25_PRN232_SE1731_ASM1_SE171916_DuongLNT/SmokeQuit.APIServices.BE.DuongLNT/Controllers/LeaderboardsDuongLntController.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SmokeQuit.Repositories.DuongLNT.ModelExtensions;
 using SmokeQuit.Repositories.DuongLNT.Models;
 using SmokeQuit.Services.DuongLNT;
-using System.Threading.Tasks;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -45,7 +44,12 @@ namespace SmokeQuit.APIServices.BE.DuongLNT.Controllers
 		[HttpPost]
 		public async Task<int> Post(LeaderboardsDuongLnt leaderboards)
 		{
-			return await _leaderboardsDuongLntService.CreateAsync(leaderboards);
+			if (ModelState.IsValid)
+			{
+				return await _leaderboardsDuongLntService.CreateAsync(leaderboards);
+			}
+
+			return 0;
 		}
 
 		// PUT api/<LeaderboardsDuongLntController>/5
@@ -54,7 +58,13 @@ namespace SmokeQuit.APIServices.BE.DuongLNT.Controllers
 		[HttpPut]
 		public async Task<int> Put(LeaderboardsDuongLnt leaderboards)
 		{
-			return await _leaderboardsDuongLntService.UpdateAsync(leaderboards);
+			if (ModelState.IsValid)
+			{
+				return await _leaderboardsDuongLntService.UpdateAsync(leaderboards);
+			}
+
+			return 0;
+
 		}
 
 		// DELETE api/<LeaderboardsDuongLntController>/5
@@ -65,27 +75,44 @@ namespace SmokeQuit.APIServices.BE.DuongLNT.Controllers
 			return await _leaderboardsDuongLntService.DeleteAsync(id);
 		}
 
-		[Authorize(Roles ="1,2")]
-		[HttpGet("{page}/{pageSize}")]
-		public async Task<IActionResult> Get(int page, int pageSize)
+		#region Comment tạm
+		//[Authorize(Roles ="1,2")]
+		//[HttpGet("{page}/{pageSize}")]
+		//public async Task<IActionResult> Get(int page, int pageSize)
+		//{
+		//	var leaderboardsQueryable =  (await _leaderboardsDuongLntService.GetAllAsync()).AsQueryable();
+
+		//	var totalCount = leaderboardsQueryable.Count();
+		//	var totalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+
+		//	leaderboardsQueryable = leaderboardsQueryable.Skip((page - 1) * pageSize).Take(pageSize);
+
+		//	var result = new
+		//	{
+		//		TotalItems = totalCount,
+		//		TotalPages = totalPages,
+		//		CurrentPage = page,
+		//		PageSize = pageSize,
+		//		LeaderBoards = leaderboardsQueryable.ToList()
+		//	};
+
+		//	return Ok(result);
+		//}
+		#endregion
+
+		[HttpGet("{note}/{money}/{reason}/{currentPage}/{pageSize}")]
+		[Authorize(Roles = "1,2")]
+		public async Task<PaginationResult<List<LeaderboardsDuongLnt>>> Get(string note, double money, string reason, int currentPage, int pageSize)
 		{
-			var leaderboardsQueryable =  (await _leaderboardsDuongLntService.GetAllAsync()).AsQueryable();
-
-			var totalCount = leaderboardsQueryable.Count();
-			var totalPages = (int)Math.Ceiling((double)totalCount / pageSize);
-
-			leaderboardsQueryable = leaderboardsQueryable.Skip((page - 1) * pageSize).Take(pageSize);
-
-			var result = new
-			{
-				TotalItems = totalCount,
-				TotalPages = totalPages,
-				CurrentPage = page,
-				PageSize = pageSize,
-				LeaderBoards = leaderboardsQueryable.ToList()
-			};
-
-			return Ok(result);
+			return await _leaderboardsDuongLntService.SearchWithPagingAsync(note, money, reason, currentPage, pageSize);
 		}
+
+		[HttpGet("{currentPage}/{pageSize}")]
+		[Authorize(Roles = "1,2")]
+		public async Task<PaginationResult<List<LeaderboardsDuongLnt>>> Get(int currentPage, int pageSize)
+		{
+			return await _leaderboardsDuongLntService.GetAllWithPagingAsync(currentPage, pageSize);
+		}
+
 	}
 }
